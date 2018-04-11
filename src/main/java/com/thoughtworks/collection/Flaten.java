8fca7b0c400f898905ebeb3ecdac9b427aa0ee82
niubilityxiesong <563527588@qq.com>
2018-04-11 15:43:06 +0800

    FlatenTest

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flaten {
@@ -12,10 +13,38 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resulttransform = new ArrayList<>();
+
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                resulttransform.add(array[i][j]);
+            }
+        }
+        return resulttransform;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resultdimesional = new ArrayList<>();
+        int i,j;
+        int sizearry;
+
+        for(i = 0; i < array.length; i++){
+            for(j = 0; j < array[i].length; j++){
+                resultdimesional.add(array[i][j]);
+            }
+        }
+
+        sizearry = resultdimesional.size();
+        for(i = 0; i < sizearry; i++){
+            for(j = i + 1; j < sizearry; j++){
+                if(resultdimesional.get(i) == resultdimesional.get(j)){
+                    resultdimesional.remove(j);
+                    --j;
+                    --sizearry;
+                }
+            }
+        }
+        return resultdimesional;
     }
 }
