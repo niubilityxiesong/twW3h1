58f64f3e6799eb23dea60a493d240640de9e1d90
niubilityxiesong <563527588@qq.com>
2018-04-11 16:40:36 +0800

    MyMap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MyMap {
 
@@ -17,22 +16,64 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> resulttriple = new ArrayList<>();
+        int temp;
+
+        for(int i = 0; i < array.size(); i++){
+            temp = array.get(i);
+            temp = temp * 3;
+            resulttriple.add(temp);
+        }
+        return resulttriple;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> resultmap = new ArrayList<>();
+
+        for(int i = 0; i < array.size(); i++){
+            resultmap.add(letterList.get(array.get(i) - 1));
+        }
+        return resultmap;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> resultmaps = new ArrayList<>();
+        String temp = new String();
+        int firstarry,secendarry;
+
+        for(int i = 0; i < array.size(); i++){
+            if(array.get(i) < 27){
+                temp = letterList.get(array.get(i) - 1);
+            }
+            else {
+                firstarry = array.get(i) / 26;
+                secendarry = array.get(i) % 26;
+                if(secendarry != 0){
+                    temp = letterList.get(firstarry - 1) + letterList.get(secendarry - 1);
+                }
+                else {
+                    temp = letterList.get(firstarry - 2) + letterList.get(25);
+                }
+
+            }
+            resultmaps.add(temp);
+        }
+        return resultmaps;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> resultsort = new ArrayList<>();
+
+        resultsort.addAll(array);
+        Collections.reverse(resultsort);
+        return resultsort;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        List<Integer> resultsort = new ArrayList<>();
+
+        resultsort.addAll(array);
+        Collections.sort(resultsort);
+        return resultsort;
     }
 }
