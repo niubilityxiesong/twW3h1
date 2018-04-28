10fb0c61b0a45eb620eaa740d563b04ae9d3fb8c
niubilitisong <66543775xie>
2018-04-28 12:04:28 +0800

    use stream() to finish Filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -3,8 +3,10 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -38,33 +40,36 @@ public class Filter {
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        List<Integer> resultcomment = new ArrayList<>();
-        int sizearry = firstList.size();
-
-        for(int i = 0; i < sizearry; i++){
-            for(int j = 0; j < sizearry; j++){
-                if(firstList.get(i) == secondList.get(j)){
-                    resultcomment.add(firstList.get(i));
-                }
-            }
-        }
-        return resultcomment;
+      //  List<Integer> resultcomment = new ArrayList<>();
+      //  int sizearry = firstList.size();
+//
+      //  for(int i = 0; i < sizearry; i++){
+      //      for(int j = 0; j < sizearry; j++){
+      //          if(firstList.get(i) == secondList.get(j)){
+      //              resultcomment.add(firstList.get(i));
+      //          }
+      //      }
+      //  }
+      //  return resultcomment;
+        return firstList.stream().filter(secondList::contains).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        List<Integer> resultdifferent = new ArrayList<>();
-        int sizearry = array.size();
+       // List<Integer> resultdifferent = new ArrayList<>();
+       // int sizearry = array.size();
+//
+       // resultdifferent.addAll(array);
+       // for(int i = 0; i < sizearry; i++){
+       //     for(int j = i + 1; j < sizearry; j++){
+       //         if(resultdifferent.get(i) == resultdifferent.get(j)){
+       //             resultdifferent.remove(j);
+       //             --j;
+       //             --sizearry;
+       //         }
+       //     }
+       // }
+       // return resultdifferent;
 
-        resultdifferent.addAll(array);
-        for(int i = 0; i < sizearry; i++){
-            for(int j = i + 1; j < sizearry; j++){
-                if(resultdifferent.get(i) == resultdifferent.get(j)){
-                    resultdifferent.remove(j);
-                    --j;
-                    --sizearry;
-                }
-            }
-        }
-        return resultdifferent;
+        return new ArrayList<>(new HashSet<>(array));
     }
 }
